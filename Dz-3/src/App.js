03c66e8cef44fbcc9feea93962abf072013237ee
DenTheProgrammer <den4ikk007@gmail.com>
2018-10-27 20:38:37 +0300

    init

---
+++ Dz-3/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+//
+// let date = document.querySelector("#date");
+// ReactDom.render(<h1 onClick={()=>alert(new Date())}>Вывод Даты</h1>,date);
